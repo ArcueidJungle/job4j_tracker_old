288e47b79ef38d699ac19e9cf6346ae8f3a51d3f
kamin <kaminoonte00@gmail.com>
2022-03-20 00:33:19 +0300

    1.2. Вызов метода объекта [#166085 #277132]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,10 +1,6 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void music() {
-        System.out.println("Tra tra tra");
-    }
-
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -13,6 +9,10 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
+    }
+
+    public void music() {
+        System.out.println("Tra tra tra");
     }
 
     public void song() {
