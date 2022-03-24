05fb62a5697321dc1c3f563f6b9f0ba41e998be0
kamin <kaminoonte00@gmail.com>
2022-03-24 21:01:20 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,29 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book iron = new Book("iron", 10);
+        Book history = new Book("history", 5);
+        Book physcho = new Book("psycho", 2);
+        Book cleanCode = new Book("Clean code", 1);
+        Book[] lib = new Book[4];
+        lib[0] = iron;
+        lib[1] = history;
+        lib[2] = physcho;
+        lib[3] = cleanCode;
+        for (Book bk : lib) {
+            System.out.println(bk.getName() + '-' + bk.getCount());
+        }
+        Book temp = lib[0];
+        lib[0] = lib[3];
+        lib[3] = temp;
+        for (Book bk : lib) {
+            System.out.println(bk.getName() + '-' + bk.getCount());
+        }
+        for (Book bk : lib) {
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + '-' + bk.getCount());
+            }
+        }
+     }
+}
